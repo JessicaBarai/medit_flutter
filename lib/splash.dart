99e6a476a532ba636900c65d21eb8a5955d4c314
Min Dator <mejlbarai@gmail.com>
2023-07-08 08:46:42 +0200

    initial commit Flutter project

---
+++ lib/splash.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+ // consider using another type since this increase time for user when starting app
+class Splash extends StatefulWidget{
+  const Splash ({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+
+  }
+
+  _navigatetohome()async{
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> MyHomePage(title: "MedIt")));
+  }
+
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("Spalsh screen", 
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+            ),),
+        ),
+      ),
+    );
+  }
+}
