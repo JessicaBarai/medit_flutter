5bcbf06c320c6113ac29b763b1a440f40fd23af4
Min Dator <mejlbarai@gmail.com>
2023-07-23 09:35:04 +0200

    added example home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,38 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/utils/smart_device_box.dart';
 
-class HomePage extends StatelessWidget{
-   HomePage({super.key});
+class HomePage extends StatefulWidget{
+   const HomePage({super.key});
+
+ @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
 
 final double horizontalpadding = 40;
 final double verticalpadding = 25;
-  final user = FirebaseAuth.instance.currentUser!;
+final user = FirebaseAuth.instance.currentUser!;
 
 void signUserOut(){
   FirebaseAuth.instance.signOut();
 }
+
+  void powerSwitchChanged(bool value, int index) {
+    setState(() {
+      mySmartDevices[index][2] = value;
+    });
+  }
+
+  // list of smart devices
+  List mySmartDevices = [
+    // [ smartDeviceName, iconPath , powerStatus ]
+    ["Smart Light", "lib/images/light-bulb.png", true],
+    ["Smart AC", "lib/images/air-conditioner.png", false],
+    ["Smart TV", "lib/images/smart-tv.png", false],
+    ["Smart Fan", "lib/images/fan.png", false],
+  ];
 
 
 @override
@@ -52,8 +74,40 @@ Widget build(BuildContext context){
          style: TextStyle(fontSize:40),),
       ]
     ),
-  )
-   
+  ),
+
+  
+
+
+
+  const SizedBox(height: 20),
+
+  Padding(padding: EdgeInsets.symmetric(horizontal: horizontalpadding),
+  child: Text("Smart Device"),
+  ),
+
+     // grid
+            Expanded(
+              child: GridView.builder(
+                itemCount: 4,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1 / 1.3,
+                ),
+                itemBuilder: (context, index) {
+                    return SmartDeviceBox(
+                    smartDeviceName: mySmartDevices[index][0],
+                    iconPath: mySmartDevices[index][1],
+                    powerOn: mySmartDevices[index][2],
+                    onChanged: (value) => powerSwitchChanged(value, index),
+                  );
+                },
+              ),
+            )
+    
+
 
     ],
     )
@@ -62,7 +116,4 @@ Widget build(BuildContext context){
   );
 
 }
-
-
-
 }
